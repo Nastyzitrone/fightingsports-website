cd8a0adf2e61fd6f0677ee416eb55bb1046dd38b
Nastyzitrone <viktorgoeber@gmail.com>
2022-03-03 17:42:52 +0100

    connect controller and model

--- controller/MainFilterController.php
+++ controller/MainFilterController.php
@@ -1,14 +1,17 @@
 <?php
-
+require_once('../models/FightingSportModel.php');
 // require model
 class MainFilterController {
 
-    public function __construct(){
+    private $model = null; 
 
+    public function __construct(){
+        $this->model = new FightingSportModel();
     }
     
     public function showData($postData){
-        $postData['test'] = 'Hallo Welt';
+        $postData['test'] = $this->model->test();
         return "here rin controller".json_encode($postData);
+        
     }
 }
