c3a5ebd7cc5831a230a939447f3e1b385bb10456
Nastyzitrone <viktorgoeber@gmail.com>
2022-03-03 16:06:51 +0100

    serialize form data and add form action value

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,11 +1,9 @@
 $(document).ready(function () {
     $("form").submit(function (event) {
-      var formData = {
-          action: 'filter',
-        fname: $("#fname").val(),
-        lname: $("#lname").val()
-      };
-  
+      var formData = $("#form1").serializeArray();
+      formData.push({name: 'action' , value: $("#form1").attr('action')});
+      
+      console.log(formData);
       $.ajax({
         type: "POST",
         url: "../controller/StartController.php",
